8fe504d83da4283033d24e9b92e2b104f454d02f
Marcos Siqueira Junior <marcosjunior182@gmail.com>
2020-01-06 18:41:22 -0400

    fix: tslint on template schema

--- src/templates/schemas/template.schema.ts
+++ src/templates/schemas/template.schema.ts
@@ -1,33 +1,33 @@
 import * as mongoose from 'mongoose';
 
-const TemplateStatus = { 
+const TemplateStatus = {
   type: String,
   enum: ['draft', 'saved', 'published'],
-  default: 'draft'
-}
+  default: 'draft',
+};
 
 const TemplateComponentValue = {
   name: { type: String, required: true },
-  value: mongoose.Schema.Types.Mixed
-}
+  value: mongoose.Schema.Types.Mixed,
+};
 
 const TemplateComponent = {
   name:  { type: String, required: true },
-  value: TemplateComponentValue
-}
+  value: TemplateComponentValue,
+};
 
 export const TemplateSchema = new mongoose.Schema({
   description: { type: String, default: '' },
   version:     { type: String, required: true, unique: true },
   status:      TemplateStatus,
-  
+
   // Template components
   components:  [TemplateComponent],
-  
+
   // Domain's information
   info: {
     client:   { type: String, required: true },
     subject:  { type: String, required: true },
-    campaign: { type: String, required: true }
-  }
+    campaign: { type: String, required: true },
+  },
 });
